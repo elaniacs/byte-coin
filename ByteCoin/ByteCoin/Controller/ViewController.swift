85641de7e952bfc5015ca4267b8134960c00ba88
elaniacs <carensousaesilva@gmail.com>
2022-09-28 10:53:38 -0300

    Adopting Protocol UIPickerViewDataSource and implementing methods

--- ByteCoin/ByteCoin/Controller/ViewController.swift
+++ ByteCoin/ByteCoin/Controller/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var bitcoinLabel: UIStackView!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    let coinManager = CoinManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
     }
-
-
+    
+    /* determine how many columns we want in our picker. */
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    /* how many rows this picker should have using */
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    /* update the PickerView with some titles and detect when it is interacted with. */
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    /* called every time the user scrolls the picker, recording the number of the line that was selected. */
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(coinManager.currencyArray[row])
+    }
 }
+
+
+
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # byte-coin
+
+UIPickerViewDataSource: This says that the ViewController class is capable of providing data to any UIPickerViews.
